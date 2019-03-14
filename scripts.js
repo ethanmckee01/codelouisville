8e4a686d755dc37ddd1408a0b7a31286b49cefcb
ethan mckee <barbwirebill@gmail.com>
2019-03-14 00:11:56 -0400

    added comments

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
+//get the DOM ready
+
 $( document ).ready(function() 
 {
+    //event handler for lanthanide flex box
+
     $(".lanel").on("click",function(event){
-        console.log(event.target.textContent());
+        console.log(event.target.innerText);
+    });
+
+    //event handler for actinide flex box
+    
+    $(".actel").on("click",function(event){
+        console.log(event.target.innerText);
     });
     });
